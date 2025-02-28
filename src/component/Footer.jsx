dd04cf9e41050c32638c49717acffccce2801a38
Kingshuk Hazra <kingshukhazra5@gmail.com>
2025-03-01 04:24:22 +0530

    add footer component and enhance navbar and about section styling

---
+++ src/component/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <p>&copy; 2025 TSP NewsBit. All rights reserved by Krishnendu & Kingshuk.</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/component/NewsCard.jsx
+++ src/component/NewsCard.jsx
@@ -4,15 +4,20 @@ import template from "../assets/news_template.jpeg";
 const NewsCard = ({ news }) => {
     return (
         <div className="news-card">
-        <a href={news.url} target="_blank" rel="noopener noreferrer">
-            <img src={news.urlToImage} alt={news.title} className="news-image" />
-            <div className="news-details">
-            <h2 className="news-title">{news.title}</h2>
-            <p className="news-author">By: {news.author || "Unknown"}</p>
-            <p className="news-date">Published: {news.publishedAt.slice(0, 10)}</p>
-            <p className="news-description">{news.description}</p>
-            </div>
-        </a>
+            <a href={news.url} target="_blank" rel="noopener noreferrer">
+                <img 
+                    src={news.urlToImage} 
+                    alt={news.title} 
+                    className="news-image" 
+                    onError={(e) => e.target.src = template} 
+                />
+                <div className="news-details">
+                    <h2 className="news-title">{news.title}</h2>
+                    <p className="news-date">Published: {news.publishedAt.slice(0, 10)}</p>
+                    <p className="news-author">By: {news.author || "Unknown"}</p>
+                    <p className="news-description">{news.description}</p>
+                </div>
+            </a>
         </div>
     );
 };
