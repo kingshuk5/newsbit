7fe98a0a138b30252ed87fa16a484c0d73a0750b
Kingshuk Hazra <kingshukhazra5@gmail.com>
2025-03-02 20:34:33 +0530

    update navbar styling, enhance news card layout, and improve API fetch logic

--- src/api.js
+++ src/api.js
@@ -1,8 +1,11 @@
-const API_KEY = "a885b175578c4d5386452982023dafd3";
-const BASE_URL = "https://newsapi.org/v2";
+// const API_KEY = "a885b175578c4d5386452982023dafd3";
+// const BASE_URL = "https://newsapi.org/v2";
 
 export const fetchTrendingMovies = async () => {
-    const response = await fetch(`${BASE_URL}/everything?q=tesla&from=2025-01-28&sortBy=publishedAt&apiKey=${API_KEY}`);
+    const response = await fetch(`https://newsapi.org/v2/everything?q=tesla&from=2025-02-02&sortBy=publishedAt&apiKey=a885b175578c4d5386452982023dafd3`);
     const data = await response.json();
     return data.articles;
 }
+
+
+// https://newsapi.org/v2/everything?q=tesla&from=2025-02-02&sortBy=publishedAt&apiKey=a885b175578c4d5386452982023dafd3
